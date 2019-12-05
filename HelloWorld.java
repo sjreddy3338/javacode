53a5a76e74c45df6a07719cfcd422320c89e17ae
sjreddy3338 <sjreddy3338@gmail.com>
2019-12-05 21:22:19 +0530

    done

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("Hello, India");
+    System.out.println("Hello, ap");
+    System.out.println("Hello, kadapa");
+
+  }
+}
